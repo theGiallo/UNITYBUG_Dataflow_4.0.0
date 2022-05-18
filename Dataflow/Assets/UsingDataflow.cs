54b6639fec8654721e014a9a4945351a27c11c5c
Gianluca Alloisio <g.alloisio@xplored.com>
2022-05-18 09:58:30 +0200

    Not using Dataflow, dll excluded from Android - OK
    The normal case still works.

--- Dataflow/Assets/UsingDataflow.cs
+++ Dataflow/Assets/UsingDataflow.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading.Tasks.Dataflow;
 
 public class UsingDataflow : MonoBehaviour
 {
-	public BufferBlock<float> bufferBlock;
 }
